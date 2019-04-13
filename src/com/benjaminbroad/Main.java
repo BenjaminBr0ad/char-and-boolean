268c8616a2ea9429110ae475f43c1706203ae972
Benjamin J. Broad <benjamin.broad@colorado.edu>
2019-04-13 16:11:23 -0600

    Char and Boolean practice in Java.

---
+++ src/com/benjaminbroad/Main.java
@@ -0,0 +1,25 @@
+package com.benjaminbroad;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Stores one letter or one character
+        // Width of 16 (2 bytes)
+        char myChar1 = 'D';
+        char myChar2 = '$';
+        char myChar3 = '1';
+        // Unicode character example
+        char myChar4 = '\u00A9';
+        char myChar5 = '\u00AE';
+
+        System.out.println(myChar4);
+        System.out.println(myChar5);
+
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+
+        System.out.println(!myBoolean2);
+
+
+    }
+}
